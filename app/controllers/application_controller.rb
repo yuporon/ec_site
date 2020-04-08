b65b8d309bc04d6e6feca57054baae8ee4040c00
ftbyu <ftb.y.u.kashima.07outlook.jp>
2020-04-08 08:35:36 +0000

    fix end_user/show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def after_sign_in_path_for(resource)
         case resource
         when Admin
@@ -13,5 +15,10 @@ class ApplicationController < ActionController::Base
         elsif resource_or_scope == :end_user
           new_end_user_session_path
         end
-      end
+    end
+
+    protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name])
+    end
 end
--- app/controllers/publics/end_users_controller.rb
+++ app/controllers/publics/end_users_controller.rb
@@ -1,5 +1,10 @@
 class Publics::EndUsersController < ApplicationController
     before_action :authenticate_end_user!
+    # before_action :correct_user, only: [:edit]
+
     def show
     end
+    def edit
+    end
+
 end
