73a467ab0ade7de94a0b772eb9399c452f8ee460
ftbyu <ftb.y.u.kashima.07outlook.jp>
2020-04-25 21:42:07 +0000

    Fix admin/genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -11,9 +11,17 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "genre was successfully created."
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
   end
   private
   def genre_params
